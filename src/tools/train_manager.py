d87a642483c0d56ec23735b5822c816fcfe99dfd
Thomas Phillips <tphillips2020@gmail.com>
2024-03-15 00:21:18 +1030

    Updated train manager to allow for new dataset

--- src/tools/train_manager.py
+++ src/tools/train_manager.py
@@ -29,7 +29,6 @@ class TrainManager:
         self.train_dataloader = train_dataloader
         self.validation_dataloader = validation_dataloader
         self.device = device
-        self.writer = writer
         self.metrics = metrics
         self.reference_metric = reference_metric
         self.epochs = epochs
@@ -53,23 +52,24 @@ class TrainManager:
         self.model.train()
         step = epoch * len(self.train_dataloader)
         train_loss = 0
-        for input_data, target_data in tqdm(
-            self.train_dataloader,
-            desc=f"Train",
-            bar_format="{l_bar}{bar:10}{r_bar}{bar:-10b}",
+        for batch_idx, (batch_spectrograms, labels) in enumerate(
+            tqdm(
+                self.train_dataloader,
+                desc=f"Train",
+                bar_format="{l_bar}{bar:10}{r_bar}{bar:-10b}",
+            )
         ):
 
-            input_data = input_data.to(self.device)
-            target_data = target_data.to(self.device)
+            batch_spectrograms = batch_spectrograms.to(self.device)
+            labels = labels.to(self.device)
 
             # calculate loss
             self._efficient_zero_grad(self.model)
-            prediction = self.model(input_data)
-            loss = self.loss_fn(prediction, target_data)
+            prediction = self.model(batch_spectrograms.unsqueeze(1).float())
+            loss = self.loss_fn(prediction, labels)
             train_loss += loss.item()
 
             step += 1
-            # self.writer.add_scalar("Loss/train", loss, step)
 
             # backpropagate error and update weights
             loss.backward()
@@ -77,7 +77,6 @@ class TrainManager:
 
         train_loss = train_loss / len(self.train_dataloader)
 
-        self.writer.add_scalar("Loss/train_epoch", train_loss, step)
         print(f"Loss: {train_loss:.4f}")
         return train_loss
 
@@ -86,22 +85,24 @@ class TrainManager:
         self.last_validation_loss = self.current_validation_loss
         self.current_validation_loss = 0
         display_values = []
-        for input_data, target_data in tqdm(
-            self.validation_dataloader,
-            desc=f"Validation",
-            bar_format="{l_bar}{bar:10}{r_bar}{bar:-10b}",
+        for batch_idx, (batch_spectrograms, labels) in enumerate(
+            tqdm(
+                self.validation_dataloader,
+                desc=f"Validation",
+                bar_format="{l_bar}{bar:10}{r_bar}{bar:-10b}",
+            )
         ):
+            batch_spectrograms = batch_spectrograms.to(self.device)
+            labels = labels.to(self.device)
 
-            input_data = input_data.to(self.device)
-            target_data = target_data.to(self.device)
-
-            prediction = self.model(input_data)
-
-            loss = self.loss_fn(prediction, target_data)
+            prediction = self.model(batch_spectrograms.unsqueeze(1).float())
+            loss = self.loss_fn(prediction, labels)
             self.current_validation_loss += loss.item()
 
-            # for metric in self.metrics:
-            #     self.metrics[metric](prediction, target_data)
+            labels = labels.to("cpu")
+            prediction = prediction.to("cpu")
+            for metric in self.metrics:
+                self.metrics[metric](prediction, labels)
 
         use_reference = False
         if self.reference_metric in self.metrics:
@@ -110,31 +111,27 @@ class TrainManager:
         self.current_validation_loss = self.current_validation_loss / len(
             self.validation_dataloader
         )
-        self.writer.add_scalar(f"Loss/validation", self.current_validation_loss, epoch)
         display_values.append(f"Loss: {self.current_validation_loss:.4f}")
 
-        # for idx, metric in enumerate(self.metrics):
-        #     value = self.metrics[metric].compute()
-        #     if idx == 0:
-        #         ref_metric = value
-        #     if use_reference:
-        #         if metric == self.reference_metric:
-        #             ref_metric = value
-        #     if metric == "ConfusionMatrix":
-        #         cm_fig = plot_confusion_matrix(
-        #             value.numpy(),
-        #             class_names=self.validation_dataloader.dataset.class_mapping.keys(),
-        #         )
-        #         self.writer.add_figure(f"Metrics/{metric}", cm_fig, epoch)
-        #     else:
-        #         display_values.append(f"{metric}: {value:.4f}")
-        #         self.writer.add_scalar(f"Metrics/{metric}", value, epoch)
-        #     self.metrics[metric].reset()
+        for idx, metric in enumerate(self.metrics):
+            value = self.metrics[metric].compute()
+            if idx == 0:
+                ref_metric = value
+            if use_reference:
+                if metric == self.reference_metric:
+                    ref_metric = value
+            if metric == "ConfusionMatrix":
+                cm_fig = plot_confusion_matrix(
+                    value.numpy(),
+                    class_names=self.validation_dataloader.dataset.classes,
+                )
+            else:
+                display_values.append(f"{metric}: {value:.4f}")
+            self.metrics[metric].reset()
 
         print("  ".join(display_values))
 
-        # return ref_metric
-        return ""
+        return ref_metric
 
     def start_train(self, checkpoint_manager=None):
         for epoch in range(self.initial_epoch, self.epochs):
@@ -142,11 +139,9 @@ class TrainManager:
             loss = self._train_single_epoch(epoch)
             measure = self._validate_single_epoch(epoch)
 
-            self.writer.add_scalar(
-                f"Hyper/lr", self.optimizer.param_groups[0]["lr"], epoch
-            )
             self.lr_scheduler.step()
 
+            print(measure)
             if measure.cpu().detach().numpy() > self.best_measure:
                 self.best_measure = measure.cpu().detach().numpy()
 
