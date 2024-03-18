b8af3a94f74498c780d478ee51cbc9158973deac
Thomas Phillips <tphillips2020@gmail.com>
2024-03-18 10:59:22 +0000

    Created new dataset and model to represent paper fully

--- src/one_stage/new_dataset.py
+++ src/one_stage/new_dataset.py
@@ -6,9 +6,10 @@ from torch.utils.data import Dataset
 
 
 class SpectrogramDataset(Dataset):
-    def __init__(self, data_dir, preprocessing_method="mel"):
+    def __init__(self, data_dir, preprocessing_method="mel", excluded_classes=[]):
         self.data_dir = os.path.join(data_dir, preprocessing_method)
-        self.classes = os.listdir(self.data_dir)
+        self.excluded_classes = excluded_classes
+        self.classes = self._get_classes()
         self.class_to_idx = {cls_name: i for i, cls_name in enumerate(self.classes)}
         self.data = self._load_data()
 
@@ -20,7 +21,16 @@ class SpectrogramDataset(Dataset):
             idx = idx.tolist()
 
         spectrogram, label = self.data[idx]
+        spectrogram = np.expand_dims(spectrogram, axis=0)
+        # if len(spectrogram.shape) == 2:
+        #     # If single-channel, convert to three-channel by repeating the channel dimension
+        #     spectrogram = np.repeat(spectrogram[:, :, np.newaxis], 3, axis=2)
+        #     spectrogram = np.transpose(spectrogram, (2, 0, 1))
+
         return spectrogram, label
+
+    def _get_classes(self):
+        return [c for c in os.listdir(self.data_dir) if c not in self.excluded_classes]
 
     def _load_data(self):
         data = []
