cd90f04b6c1fcf2b480343018683dc405a1a88c0
Thomas Phillips <tphillips2020@gmail.com>
2024-03-18 10:59:10 +0000

    Fixed fully connected layers and improved formatting

--- src/one_stage/model.py
+++ src/one_stage/model.py
@@ -1,5 +1,6 @@
 from torch import nn
 
+
 class FeedForwardNet(nn.Module):
     """The standard FC approach to the Underwater
     Classification problem.
@@ -9,9 +10,7 @@ class FeedForwardNet(nn.Module):
         super().__init__()
         self.flatten = nn.Flatten()
         self.dense_layers = nn.Sequential(
-            nn.Linear(64 * 63, 256),
-            nn.ReLU(),
-            nn.Linear(256, 5)
+            nn.Linear(64 * 63, 256), nn.ReLU(), nn.Linear(256, 5)
         )
         self.softmax = nn.Softmax(dim=1)
 
@@ -37,47 +36,35 @@ class CNNNetwork(nn.Module):
                 out_channels=16,
                 kernel_size=3,
                 stride=1,
-                padding=2
+                padding=2,
             ),
             nn.BatchNorm2d(16),
             nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2)
+            nn.MaxPool2d(kernel_size=2),
         )
         self.conv2 = nn.Sequential(
             nn.Conv2d(
-                in_channels=16,
-                out_channels=32,
-                kernel_size=3,
-                stride=1,
-                padding=2
+                in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=2
             ),
             nn.BatchNorm2d(32),
             nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2)
+            nn.MaxPool2d(kernel_size=2),
         )
         self.conv3 = nn.Sequential(
             nn.Conv2d(
-                in_channels=32,
-                out_channels=64,
-                kernel_size=3,
-                stride=1,
-                padding=2
+                in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=2
             ),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2)
+            nn.MaxPool2d(kernel_size=2),
         )
         self.conv4 = nn.Sequential(
             nn.Conv2d(
-                in_channels=64,
-                out_channels=128,
-                kernel_size=3,
-                stride=1,
-                padding=2
+                in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=2
             ),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2)
+            nn.MaxPool2d(kernel_size=2),
         )
 
         linear_dim = 0
@@ -86,19 +73,19 @@ class CNNNetwork(nn.Module):
             linear_dim = 64 * 9 * 9
         elif self.depth == 4:
             self.conv_layers = [self.conv2, self.conv3, self.conv4]
-            #linear_dim = 128 * 5 * 5
-            linear_dim = 128 * 7 * 9 # best until now
+            # linear_dim = 128 * 5 * 5
+            linear_dim = 128 * 7 * 9  # best until now
 
         self.linear = nn.Sequential(
             nn.Flatten(),
             nn.Linear(linear_dim, out_classes, bias=False),
             nn.Dropout(p=0.1),
-            nn.LeakyReLU()
+            nn.LeakyReLU(),
         )
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, input_data):
-        
+
         x = self.conv1(input_data)
         for layer in self.conv_layers:
             x = layer(x)
@@ -112,7 +99,7 @@ class CNNNetworkCQT(nn.Module):
     Classification problem using CQT.
     """
 
-    def __init__(self, model_depth=0, input_channels=1):
+    def __init__(self, model_depth=0, input_channels=1, out_classes=5):
         super().__init__()
         # 4 conv blocks / flatten / linear / softmax
         self.conv1 = nn.Sequential(
@@ -121,46 +108,34 @@ class CNNNetworkCQT(nn.Module):
                 out_channels=16,
                 kernel_size=3,
                 stride=1,
-                padding=2
+                padding=2,
             ),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2)
+            nn.MaxPool2d(kernel_size=2),
         )
         self.conv2 = nn.Sequential(
             nn.Conv2d(
-                in_channels=16,
-                out_channels=32,
-                kernel_size=3,
-                stride=1,
-                padding=2
+                in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=2
             ),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2)
+            nn.MaxPool2d(kernel_size=2),
         )
         self.conv3 = nn.Sequential(
             nn.Conv2d(
-                in_channels=32,
-                out_channels=64,
-                kernel_size=3,
-                stride=1,
-                padding=2
+                in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=2
             ),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2)
+            nn.MaxPool2d(kernel_size=2),
         )
         self.conv4 = nn.Sequential(
             nn.Conv2d(
-                in_channels=64,
-                out_channels=128,
-                kernel_size=3,
-                stride=1,
-                padding=2
+                in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=2
             ),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2)
+            nn.MaxPool2d(kernel_size=2),
         )
         self.flatten = nn.Flatten()
-        self.linear = nn.Linear(128 * 5 * 9, 5)
+        self.linear = nn.Linear(128 * 7 * 9, out_classes)
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, input_data):
@@ -178,16 +153,22 @@ class ResBlock(nn.Module):
     def __init__(self, in_channels, out_channels, downsample):
         super().__init__()
         if downsample:
-            self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1)
+            self.conv1 = nn.Conv2d(
+                in_channels, out_channels, kernel_size=3, stride=2, padding=1
+            )
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=2),
-                nn.BatchNorm2d(out_channels)
+                nn.BatchNorm2d(out_channels),
             )
         else:
-            self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+            self.conv1 = nn.Conv2d(
+                in_channels, out_channels, kernel_size=3, stride=1, padding=1
+            )
             self.shortcut = nn.Sequential()
 
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(
+            out_channels, out_channels, kernel_size=3, stride=1, padding=1
+        )
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
@@ -206,28 +187,23 @@ class ResNet18(nn.Module):
             nn.Conv2d(input_channels, 64, kernel_size=7, stride=2, padding=3),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(64),
-            nn.ReLU()
+            nn.ReLU(),
         )
 
         self.layer1 = nn.Sequential(
-            ResBlock(64, 64, downsample=False),
-            ResBlock(64, 64, downsample=False)
+            ResBlock(64, 64, downsample=False), ResBlock(64, 64, downsample=False)
         )
 
         self.layer2 = nn.Sequential(
-            ResBlock(64, 128, downsample=True),
-            ResBlock(128, 128, downsample=False)
+            ResBlock(64, 128, downsample=True), ResBlock(128, 128, downsample=False)
         )
 
         self.layer3 = nn.Sequential(
-            ResBlock(128, 256, downsample=True),
-            ResBlock(256, 256, downsample=False)
+            ResBlock(128, 256, downsample=True), ResBlock(256, 256, downsample=False)
         )
 
-
         self.layer4 = nn.Sequential(
-            ResBlock(256, 512, downsample=True),
-            ResBlock(512, 512, downsample=False)
+            ResBlock(256, 512, downsample=True), ResBlock(512, 512, downsample=False)
         )
 
         self.gap = nn.AdaptiveAvgPool2d(1)
