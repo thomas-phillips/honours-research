d1291dbf23d5a845504968a2d55cd7ef673fb8da
Thomas Phillips <tphillips2020@gmail.com>
2024-03-27 03:59:49 +0000

    MaML training

--- src/meta_learning.py
+++ src/meta_learning.py
@@ -74,9 +74,16 @@ print(maml)
 train_loader = DataLoader(train_data, 4, num_workers=6, pin_memory=True)
 val_loader = DataLoader(val_data, 4, num_workers=6, pin_memory=True)
 
-for epoch in range(EPOCHS):
-    train_loader = DataLoader(train_data, 4, num_workers=6, pin_memory=True)
 
+def remove_dimension_x(x):
+    return x[:, 0, :, :, :]
+
+
+def remove_dimension_y(y):
+    return y[:, 0]
+
+
+for epoch in range(EPOCHS):
     for step, data in enumerate(train_loader):
         x_shot, x_qry, y_shot, y_qry = data[0], data[1], data[2], data[3]
 
@@ -87,5 +94,30 @@ for epoch in range(EPOCHS):
             y_qry.to("cuda"),
         )
 
+        # https://github.com/dragen1860/MAML-Pytorch/issues/41#issuecomment-600604345
+        # accuracy before the first update, accuracy after the first update, accuracies for each update steps (set in args)
         accs = maml(x_shot, y_shot, x_qry, y_qry)
-        print(epoch, step, accs)
+        # print(epoch, step, accs)
+
+        if step % 30 == 0:
+            print("step:", step, "\ttraining acc:", accs)
+
+            # if step % 500 == 0:  # evaluation
+            #     accs_all_test = []
+            #     for x_spt, x_qry, y_spt, y_qry in val_loader:
+            #         x_spt = remove_dimension_x(x_spt)
+            #         x_qry = remove_dimension_x(x_qry)
+            #         y_spt = remove_dimension_y(y_spt)
+            #         y_qry = remove_dimension_y(y_qry)
+            #         x_spt, y_spt, x_qry, y_qry = (
+            #             x_spt.squeeze(0).to(device),
+            #             y_spt.squeeze(0).to(device),
+            #             x_qry.squeeze(0).to(device),
+            #             y_qry.squeeze(0).to(device),
+            #         )
+
+            #         accs = maml.finetunning(x_spt, y_spt, x_qry, y_qry)
+            #         accs_all_test.append(accs)
+
+            #     accs = np.array(accs_all_test)
+            print("Test acc:", accs)
