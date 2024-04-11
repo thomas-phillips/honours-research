be261bcc40e4e7068b4a6f505f6574321ba57e08
Thomas Phillips <tphillips2020@gmail.com>
2024-04-11 07:28:03 +0000

    Formatting and postgres db connector

--- src/tools/utils.py
+++ src/tools/utils.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import itertools
 from glob import glob
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
 
 
 def plot_confusion_matrix(cm, class_names, normalize=True):
@@ -10,18 +14,17 @@ def plot_confusion_matrix(cm, class_names, normalize=True):
     Returns a matplotlib figure containing the plotted confusion matrix.
 
     Args:
-       cm (array, shape = [n, n]): a confusion matrix of integer classes
-       class_names (array, shape = [n]): String names of the integer classes
+        cm (array, shape = [n, n]): a confusion matrix of integer classes
+        class_names (array, shape = [n]): String names of the integer classes
     """
-    np.seterr(divide='ignore', invalid='ignore')
+    np.seterr(divide="ignore", invalid="ignore")
 
     # Normalize the confusion matrix.
     if normalize:
-        cm = np.around(cm.astype('float') / cm.sum(axis=1)
-                    [:, np.newaxis], decimals=2)
+        cm = np.around(cm.astype("float") / cm.sum(axis=1)[:, np.newaxis], decimals=2)
 
     figure = plt.figure(figsize=(9, 8))
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
     plt.title("Confusion matrix")
     plt.colorbar()
     tick_marks = np.arange(len(class_names))
@@ -29,14 +32,14 @@ def plot_confusion_matrix(cm, class_names, normalize=True):
     plt.yticks(tick_marks, class_names)
 
     # Use white text if squares are dark; otherwise black.
-    threshold = cm.max() / 2.
+    threshold = cm.max() / 2.0
 
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         color = "white" if cm[i, j] > threshold else "black"
         plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
 
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+    plt.ylabel("True label")
+    plt.xlabel("Predicted label")
     plt.tight_layout()
     return figure
 
@@ -46,16 +49,16 @@ def plot_pr_curve(cm, class_names):
     Returns a matplotlib figure containing the plotted confusion matrix.
 
     Args:
-       cm (array, shape = [n, n]): a confusion matrix of integer classes
-       class_names (array, shape = [n]): String names of the integer classes
+        cm (array, shape = [n, n]): a confusion matrix of integer classes
+        class_names (array, shape = [n]): String names of the integer classes
     """
-    np.seterr(divide='ignore', invalid='ignore')
+    np.seterr(divide="ignore", invalid="ignore")
 
     # Normalize the confusion matrix.
-    cm = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+    cm = np.around(cm.astype("float") / cm.sum(axis=1)[:, np.newaxis], decimals=2)
 
     figure = plt.figure(figsize=(8, 8))
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
     plt.title("Confusion matrix")
     plt.colorbar()
     tick_marks = np.arange(len(class_names))
@@ -63,15 +66,15 @@ def plot_pr_curve(cm, class_names):
     plt.yticks(tick_marks, class_names)
 
     # Use white text if squares are dark; otherwise black.
-    threshold = cm.max() / 2.
+    threshold = cm.max() / 2.0
 
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         color = "white" if cm[i, j] > threshold else "black"
         plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
 
     plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+    plt.ylabel("True label")
+    plt.xlabel("Predicted label")
     return figure
 
 
@@ -86,9 +89,9 @@ def get_files(d, pattern, sort=True):
     Return a list of files in a given directory.
 
     Args:
-      d (str): The path to the directory.
-      pattern (str): The wildcard to filter files with.
-      sort (bool): Whether to sort the returned list.
+        d (str): The path to the directory.
+        pattern (str): The wildcard to filter files with.
+        sort (bool): Whether to sort the returned list.
     """
     files = glob(os.path.join(d, pattern))
     files = [f for f in files if os.path.isfile(f)]
@@ -100,3 +103,15 @@ def get_files(d, pattern, sort=True):
     if sort:
         files.sort(key=lambda x: int(x.split("/")[-1].split(".")[0]))
     return files
+
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+    )
+
+    return conn
