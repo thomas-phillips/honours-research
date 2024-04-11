3cc766587212a430112bf1862c148cade86fff30
Thomas Phillips <tphillips2020@gmail.com>
2024-04-11 07:29:01 +0000

    Cleaned up process flow and added support for siamese network and transfer learning

--- src/train.py
+++ src/train.py
@@ -11,9 +11,14 @@ from torchmetrics import Accuracy, Precision, Recall, F1, ConfusionMatrix
 
 from tools.utils import create_dir
 from tools.dataset_loader import get_standard_dataset, get_maml_dataset
-from tools.checkpoint import CheckpointManager, Checkpoint
-from tools.train_manager import TrainManagerCNN, TrainManagerMaML
-from resources.model import VGGNet, ResNet18
+from tools.train_manager import TrainManagerCNN, TrainManagerMaML, TrainManagerSiamese
+from resources.model import (
+    VGGNet,
+    ResNet18,
+    ResNet18Transfer,
+    VGGNet16Transfer,
+    SiameseNetworkResNet18,
+)
 from resources.maml_configurations import make_config
 from maml.meta import Meta
 
@@ -70,80 +75,82 @@ def train(config_file):
 
     for z in ZONES:
         for preprocessing in preprocessing_methods:
-            for model_data in model_info:
-                if method == "cnn":
+            for model_name in model_info:
+                if method == "maml":
+                    maml_method(
+                        z,
+                        model_name,
+                        preprocessing,
+                        hyperparameter_info,
+                        path_info,
+                        device=device,
+                        upload_results=upload_results,
+                    )
+                else:
                     cnn_method(
                         z,
-                        model_data["name"],
-                        model_data["input_channels"],
+                        model_name,
                         preprocessing,
                         optimiser_info,
                         hyperparameter_info,
                         path_info,
-                        device,
-                        upload_results,
-                    )
-
-                elif method == "maml":
-                    maml_method(
-                        z,
-                        model_data["name"],
-                        model_data["input_channels"],
-                        preprocessing,
-                        hyperparameter_info,
-                        path_info,
-                        device,
-                        upload_results,
+                        method=method,
+                        device=device,
+                        upload_results=upload_results,
                     )
 
 
 def cnn_method(
     zone,
     model_name,
-    input_channels,
     preprocessing,
     optimiser_info,
     hyperparameter_info,
     path_info,
+    method="cnn",
     device="cuda",
     upload_results=False,
 ):
-
-    output_directory = os.path.join(
-        path_info["output_base_path"],
-        f"in_{zone[0]}_ex_{zone[1]}_{preprocessing}_{model_name}_{input_channels}_{optimiser_info['type']}_{optimiser_info['early_stop']}",
-    )
-
-    print(output_directory)
-
-    final_model_dir = create_dir(os.path.join(output_directory, "final_model"))
-    checkpoint_dir = create_dir(os.path.join(output_directory, "checkpoints"))
-
-    model = None
-    if model_name == "resnet18":
-        model = ResNet18(input_channels)
-    elif model_name == "vggnet":
-        model = VGGNet(input_channels)
-
-    model = model.cuda()
-    print(next(model.parameters()).device)
-    print("Model Architecture")
-    print(summary(model, (input_channels, 95, 126)))
-
     dataset_path = os.path.join(
         path_info["dataset_base_path"], f"inclusion_{zone[0]}_exclusion_{zone[1]}"
     )
 
     train_dataloader, validation_dataloader, _ = get_standard_dataset(
         dataset_path,
-        preprocessing,
+        preprocessing["name"],
         hyperparameter_info["batch_size"],
+        included_classes=hyperparameter_info.get("included_classes", None),
+        shot=hyperparameter_info.get("shot", 0),
+        shuffle=hyperparameter_info.get("shuffle", False),
+        siamese=hyperparameter_info.get("siamese", False),
     )
 
-    print(len(train_dataloader.dataset.data))
+    num_of_classes = len(train_dataloader.dataset.classes)
+    print(f"Num of classes: {num_of_classes}")
+    print(f"Num of examples: {len(train_dataloader.dataset.data)}")
+
+    model = None
+    if model_name == "resnet18":
+        model = ResNet18(preprocessing["channels"], num_of_classes)
+    elif model_name == "vggnet16":
+        model = VGGNet(preprocessing["channels"], num_of_classes)
+    elif model_name == "resnet18_transfer":
+        model = ResNet18Transfer(preprocessing["channels"], num_of_classes)
+    elif model_name == "vggnet16_transfer":
+        model = VGGNet16Transfer(preprocessing["channels"], num_of_classes)
+    elif model_name == "siamese_resnet18":
+        model = SiameseNetworkResNet18(preprocessing["channels"], num_of_classes)
+
+    model = model.cuda()
+    print(hyperparameter_info)
+    if method == "cnn":
+        print("Model Architecture")
+        print(summary(model, (preprocessing["channels"], 95, 126)))
 
     # Initialise loss funtion + optimizer.
-    loss_fn = nn.CrossEntropyLoss()
+    loss_fn = nn.BCELoss()
+    if not hyperparameter_info.get("siamese", None):
+        loss_fn = nn.CrossEntropyLoss()
 
     if optimiser_info["type"] == "adam":
         optimizer = torch.optim.Adam(
@@ -162,84 +169,80 @@ def cnn_method(
         optimizer, gamma=hyperparameter_info["lr_schd_gamma"]
     )
 
-    num_of_classes = len(train_dataloader.dataset.classes)
-    print(f"Num of classes: {num_of_classes}")
-    print(f"Num of examples: {len(train_dataloader.dataset.data)}")
-
-    # Initialize metrics.
-    accuracy = Accuracy("multiclass", average="macro", num_classes=num_of_classes)
-    accuracy_micro = Accuracy(average="micro", num_classes=num_of_classes)
-    accuracy_weight = Accuracy(average="weighted", num_classes=num_of_classes)
-    precision = Precision(average="macro", num_classes=num_of_classes)
-    recall = Recall(average="macro", num_classes=num_of_classes)
-    f1 = F1(average="macro", num_classes=num_of_classes)
-    confusion_matrix = ConfusionMatrix(num_classes=num_of_classes)
-
-    metrics = {
-        "Accuracy": accuracy,
-        "AccuracyMicro": accuracy_micro,
-        "AccuracyWeighted": accuracy_weight,
-        "Precision": precision,
-        "Recall": recall,
-        "F1": f1,
-        "ConfusionMatrix": confusion_matrix,
-    }
-
-    # Create a checkpoint manager.
-    checkpoint_manager = CheckpointManager(
-        Checkpoint(model, optimizer),
-        checkpoint_dir,
-        device,
-        max_to_keep=5,
-        keep_best=True,
-    )
-    last_epoch = checkpoint_manager.restore_or_initialize()
-    init_epoch = last_epoch + 1 if last_epoch != 0 else 0
-
+    train_manager = None
+    metrics = {}
     upload_info = {
         "optimiser": optimiser_info["type"],
-        "input_channels": input_channels,
+        "input_channels": preprocessing["channels"],
         "classes": train_dataloader.dataset.classes,
         "inclusion": zone[0],
         "exclusion": zone[1],
-        "preprocessing": preprocessing,
-        "type": "cnn",
+        "preprocessing": preprocessing["name"],
+        "type": method,
+        "model_name": model_name,
         **hyperparameter_info,
     }
     print(upload_info)
 
-    # Call train routine.
-    train_manager = TrainManagerCNN(
-        model=model,
-        loss_fn=loss_fn,
-        optimizer=optimizer,
-        lr_scheduler=lr_scheduler,
-        train_dataloader=train_dataloader,
-        validation_dataloader=validation_dataloader,
-        epochs=hyperparameter_info["epochs"],
-        initial_epoch=init_epoch,
-        metrics=metrics,
-        reference_metric="Accuracy",
-        device=device,
-        early_stop=optimiser_info["early_stop"],
-        upload_results=upload_results,
-        upload_info=upload_info,
-    )
+    if method == "cnn":
 
-    train_manager.train_model(checkpoint_manager)
+        # Initialize metrics.
+        accuracy = Accuracy("multiclass", average="macro", num_classes=num_of_classes)
+        accuracy_micro = Accuracy(average="micro", num_classes=num_of_classes)
+        accuracy_weight = Accuracy(average="weighted", num_classes=num_of_classes)
+        precision = Precision(average="macro", num_classes=num_of_classes)
+        recall = Recall(average="macro", num_classes=num_of_classes)
+        f1 = F1(average="macro", num_classes=num_of_classes)
+        confusion_matrix = ConfusionMatrix(num_classes=num_of_classes)
 
-    # Save the last checkpoint model.
-    torch.save(model.state_dict(), os.path.join(final_model_dir, "last.pth"))
+        metrics = {
+            "Accuracy": accuracy,
+            "AccuracyMicro": accuracy_micro,
+            "AccuracyWeighted": accuracy_weight,
+            "Precision": precision,
+            "Recall": recall,
+            "F1": f1,
+            "ConfusionMatrix": confusion_matrix,
+        }
 
-    # Save the best accuraccy model.
-    checkpoint_manager.load_best_checkpoint()
-    torch.save(model.state_dict(), os.path.join(final_model_dir, "best.pth"))
+        train_manager = TrainManagerCNN(
+            model=model,
+            loss_fn=loss_fn,
+            optimizer=optimizer,
+            lr_scheduler=lr_scheduler,
+            train_dataloader=train_dataloader,
+            validation_dataloader=validation_dataloader,
+            epochs=hyperparameter_info["epochs"],
+            initial_epoch=0,
+            metrics=metrics,
+            reference_metric="Accuracy",
+            device=device,
+            early_stop=optimiser_info["early_stop"],
+            upload_results=upload_results,
+            upload_info=upload_info,
+        )
+
+    else:
+        train_manager = TrainManagerSiamese(
+            model=model,
+            train_dataloader=train_dataloader,
+            validation_dataloader=validation_dataloader,
+            epochs=hyperparameter_info["epochs"],
+            loss_fn=loss_fn,
+            optimizer=optimizer,
+            lr_scheduler=lr_scheduler,
+            initial_epoch=0,
+            device=device,
+            upload_results=upload_results,
+            upload_info=upload_info,
+        )
+
+    train_manager.train_model()
 
 
 def maml_method(
     zone,
     model_name,
-    input_channels,
     preprocessing,
     hyperparameter_info,
     path_info,
@@ -256,10 +259,10 @@ def maml_method(
         "update_step": hyperparameter_info["update_step"],
         "update_step_test": hyperparameter_info["update_step_test"],
         "imgsz": (95, 126),
-        "imgc": input_channels,
+        "imgc": preprocessing["channels"],
     }
 
-    model_config = make_config(model_name, input_channels, 5)
+    model_config = make_config(model_name, preprocessing["channels"], 5)
     model = Meta(model_config, **maml_config).to(device)
 
     dataset_path = os.path.join(
@@ -267,7 +270,7 @@ def maml_method(
     )
     train_dataloader, validation_dataloader, _ = get_maml_dataset(
         dataset_path,
-        preprocessing,
+        preprocessing["name"],
         hyperparameter_info["episodes"],
         hyperparameter_info["n_shot"],
         hyperparameter_info["n_query"],
@@ -276,14 +279,15 @@ def maml_method(
     )
     upload_info = {
         "model": model_name,
-        "input_channels": input_channels,
+        "input_channels": preprocessing["channels"],
         "classes": train_dataloader.dataset.classes,
         "inclusion": zone[0],
         "exclusion": zone[1],
-        "preprocessing": preprocessing,
+        "preprocessing": preprocessing["name"],
         "type": "maml",
         **hyperparameter_info,
     }
+    print(upload_info)
 
     train_mananger = TrainManagerMaML(
         model,
